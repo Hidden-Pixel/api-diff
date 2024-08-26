15826add395c0fd4c3bffc89dbada2e516a25c5d
Nick Kane <nkanedev@gmail.com>
2024-08-26 15:39:43 -0400

    add: base database layer for postgres

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	github.com/golang-migrate/migrate/v4 v4.17.1
+	github.com/jackc/pgx/v5 v5.6.0
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/viper v1.19.0
@@ -15,6 +16,9 @@ require (
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -28,7 +32,9 @@ require (
 	github.com/subosito/gotenv v1.6.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- src/database/database.go
+++ src/database/database.go
@@ -1,11 +1,134 @@
 package database
 
 import (
+	"context"
 	"fmt"
 	"net/url"
+	"time"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/spf13/viper"
 )
+
+type APIVersion struct {
+	ID          int       `json:"id"`
+	VersionName string    `json:"version_name"`
+	ReleaseDate time.Time `json:"release_date"`
+	Description string    `json:"description"`
+}
+
+type APIRequest struct {
+	ID           int       `json:"id"`
+	VersionID    int       `json:"version_id"`
+	Endpoint     string    `json:"endpoint"`
+	Method       string    `json:"method"`
+	RequestBody  string    `json:"request_body"`
+	ResponseBody string    `json:"response_body"`
+	Timestamp    time.Time `json:"timestamp"`
+}
+
+type APIDiff struct {
+	ID               int       `json:"id"`
+	BaseVersionID    int       `json:"base_version_id"`
+	CompareVersionID int       `json:"compare_version_id"`
+	Endpoint         string    `json:"endpoint"`
+	Method           string    `json:"method"`
+	DiffMetric       string    `json:"diff_metric"`
+	DivergenceScore  float64   `json:"divergence_score"`
+	CreatedAt        time.Time `json:"created_at"`
+}
+
+func NewDBPool(connString string) (*pgxpool.Pool, error) {
+	dbpool, err := pgxpool.New(context.Background(), connString)
+	if err != nil {
+		return nil, err
+	}
+	return dbpool, nil
+}
+
+func InsertAPIVersion(dbpool *pgxpool.Pool, version *APIVersion) error {
+	query := `INSERT INTO api_version (version_name, release_date, description)
+	          VALUES ($1, $2, $3) RETURNING id`
+
+	err := dbpool.QueryRow(context.Background(), query, version.VersionName, version.ReleaseDate, version.Description).Scan(&version.ID)
+	return err
+}
+
+func GetAllAPIVersions(dbpool *pgxpool.Pool) ([]APIVersion, error) {
+	rows, err := dbpool.Query(context.Background(), "SELECT id, version_name, release_date, description FROM api_version")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var versions []APIVersion
+	for rows.Next() {
+		var version APIVersion
+		err := rows.Scan(&version.ID, &version.VersionName, &version.ReleaseDate, &version.Description)
+		if err != nil {
+			return nil, err
+		}
+		versions = append(versions, version)
+	}
+
+	return versions, nil
+}
+
+func InsertAPIRequest(dbpool *pgxpool.Pool, request *APIRequest) error {
+	query := `INSERT INTO api_request (version_id, endpoint, method, request_body, response_body)
+	          VALUES ($1, $2, $3, $4, $5) RETURNING id`
+
+	err := dbpool.QueryRow(context.Background(), query, request.VersionID, request.Endpoint, request.Method, request.RequestBody, request.ResponseBody).Scan(&request.ID)
+	return err
+}
+
+func GetAllAPIRequests(dbpool *pgxpool.Pool) ([]APIRequest, error) {
+	rows, err := dbpool.Query(context.Background(), "SELECT id, version_id, endpoint, method, request_body, response_body, timestamp FROM api_request")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var requests []APIRequest
+	for rows.Next() {
+		var request APIRequest
+		err := rows.Scan(&request.ID, &request.VersionID, &request.Endpoint, &request.Method, &request.RequestBody, &request.ResponseBody, &request.Timestamp)
+		if err != nil {
+			return nil, err
+		}
+		requests = append(requests, request)
+	}
+
+	return requests, nil
+}
+
+func InsertAPIDiff(dbpool *pgxpool.Pool, diff *APIDiff) error {
+	query := `INSERT INTO api_diff (base_version_id, compare_version_id, endpoint, method, diff_metric, divergence_score)
+	          VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
+
+	err := dbpool.QueryRow(context.Background(), query, diff.BaseVersionID, diff.CompareVersionID, diff.Endpoint, diff.Method, diff.DiffMetric, diff.DivergenceScore).Scan(&diff.ID)
+	return err
+}
+
+func GetAllAPIDiffs(dbpool *pgxpool.Pool) ([]APIDiff, error) {
+	rows, err := dbpool.Query(context.Background(), "SELECT id, base_version_id, compare_version_id, endpoint, method, diff_metric, divergence_score, created_at FROM api_diff")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var diffs []APIDiff
+	for rows.Next() {
+		var diff APIDiff
+		err := rows.Scan(&diff.ID, &diff.BaseVersionID, &diff.CompareVersionID, &diff.Endpoint, &diff.Method, &diff.DiffMetric, &diff.DivergenceScore, &diff.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+		diffs = append(diffs, diff)
+	}
+
+	return diffs, nil
+}
 
 func PGConnectionString() string {
 	connectionString := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable",
